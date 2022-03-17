3ac9dc0cf17483a467ca91e03b6ec6298a7b0d2a
ChengyuanSha <15cs69@queensu.ca>
2022-03-17 19:24:09 -0400

    update

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,31 +1,51 @@
 import PropTypes from 'prop-types'
+import { useState, useRef} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap'
+import { CSVLink } from "react-csv";
 
-const Button = ({color, text}) => {
-	const onClick = (e) => {
-		console.log(e)
-	}
+const AddButton = ({color, text}) => {
+  const [coffeeCSVData, setcoffeeCSVData] = useState([])
+  const csvLink = useRef() 
+
+  const getData = () => {
+    let data = localStorage.getItem('CoffeeData');
+    data = JSON.parse(data);
+    setcoffeeCSVData([data]);
+    console.log(data);
+    csvLink.current.link.click();
+    // alert("Recorded");
+  }
 
   return (
-    <button onClick={onClick} 
-    style={{backgroundColor:color}}
-    >
-        {text}
-    </button>
+    <div>
+      <Button onClick={() => getData()} 
+      style={{backgroundColor:color}}
+      >
+      {text}
+      </Button>
+      <CSVLink
+      data={coffeeCSVData}
+      filename='coffeeCSVData.csv'
+      className='hidden'
+      ref={csvLink}
+      target='_blank'
+      />
+    </div>
   )
 }
 
 
-Button.defaultProps = {
+AddButton.defaultProps = {
     color: 'steelblue'
 }
 
-Button.propType = {
+AddButton.propType = {
     text: PropTypes.string,
     color: PropTypes.string,
 }
 
-export default Button
+export default AddButton
 
 
 
